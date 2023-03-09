4a80efce58fb9f37211fc45a5061f6685238aa36
Vitor Matioski <vitor.m0520@gmail.com>
2023-03-09 10:48:22 -0300

    buffer

--- PROJETO 01/src/server.js
+++ PROJETO 01/src/server.js
@@ -2,24 +2,39 @@ import http from 'http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 
     const { method, url } = req
 
-    if (method === 'GET' && url === '/users') {
-        return res.setHeader('Content-type', 'application/json').end(JSON.stringify(users))
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
     }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        req.body = null
+    }
+
+
+    if (method === 'GET' && url === '/users') {
+        return res.setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
+    }
+
+
     if (method === 'POST' && url === '/users') {
 
+        const { name, email } = req.body
 
-        users.push({ name: 'John Doe', email: 'john@example.com' })
+        users.push({ id: 1, name, email })
 
         return res.writeHead(201).end();
     }
 
-
     return res.writeHead(404).end('not found');
-
 })
 
 server.listen(3333)
